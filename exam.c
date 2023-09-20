c4bfd22e1f89092e27f12e786ff153bfa45a4514
Mohammed Elarbi El Hattab <elhattabmohammedelarbi@gmail.com.com>
2023-09-20 15:44:31 -0700

    menu update

---
+++ exam.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int i = 0 ;
+
+int i = 0;
+
+struct taskInfo
+{
+   char id;
+   char tachetitle[100];
+   char tachestatus[100];
+   char deadline[100];
+   char description[100];
+} st[500];
+
--- final-exam.c
+++ final-exam.c
@@ -1,29 +1,66 @@
 int main() {
-    Tache taches[MAX_TACHES];
-    int nombreDeTaches = 0;
-
     int choix;
     do {
         printf("\nMenu :\n");
         printf("1. Ajouter une nouvelle tache\n");
         printf("2. Afficher toutes les taches\n");
-        printf("3. Quitter\n");
+        printf("3. Supprimer une tache \n");
+        printf("4. Rechercher une tache \n");
+        printf("5. Quitter\n");
         printf("Choix : ");
         scanf("%d", &choix);
 
         switch (choix) {
             case 1:
-                nombreDeTaches = ajouterTache(taches, nombreDeTaches);
+                ajouterTache();
                 break;
             case 2:
-                afficherToutesLesTaches(taches, nombreDeTaches);
+                afficherToutesLesTaches();
                 break;
             case 3:
+                if (nombreDeTaches > 0) {
+                    int idASupprimer;
+                    printf("Entrez l'identifiant de la tache a supprimer : ");
+                    scanf("%d", &idASupprimer);
+                    supprimerTache(idASupprimer);
+                } else {
+                    printf("La liste de taches est vide.\n");
+                }
+                break;
+            case 4:
+                if (nombreDeTaches > 0) {
+                    int choixRechercher;
+                    printf("1.chercher par id.\n");
+                    printf("2.chercher par titre.\n");
+		    printf("choix: ");
+                    scanf("%d", &choixRechercher);
+                    switch (choixRechercher) {
+                        case 1 :
+                             int idARechercher;
+                             printf("Entrez l'identifiant de la tache a rechercher : ");
+                             scanf("%d", &idARechercher);
+                             rechercherTache(idARechercher);
+                        break;
+                        
+                        case 2 :
+                             int titleRechercher;
+                             printf("Entrez le titre de la tache a rechercher : ");
+                             scanf("%d", &titleRechercher);
+                             rechercherTache(titleRechercher);
+                             break ;
+                        } 
+                }    
+                else {
+                    printf("La liste de taches est vide.\n");
+                }
+                break;
+            case 5:
                 printf("Au revoir !\n");
                 break;
             default:
                 printf("Choix invalide. Veuillez reessayer.\n");
         }
-    } while (choix != 3);
-        return 0;
+    } while (choix != 5);
+
+    return 0;
 }
